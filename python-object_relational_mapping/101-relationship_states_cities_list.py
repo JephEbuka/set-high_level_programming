02143b893237916cdfb73e74a8dac54216b6b199
JephEbuka <jephubakaeze@gmail.com>
2026-08-12 07:09:18 +0100

    Fix cities relationship listing

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""List States and their related Cities using one query."""
-
-import sys
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, joinedload
-
-from relationship_city import City
-from relationship_state import Base, State
+"""List all State objects and their related City objects."""
 
 
 if __name__ == "__main__":
+    from sys import argv
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    from relationship_city import City
+    from relationship_state import Base, State
+
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
+            argv[1],
+            argv[2],
+            argv[3]
         ),
         pool_pre_ping=True
     )
 
-    session = Session(engine)
+    Base.metadata.create_all(engine)
 
-    states = (
-        session.query(State)
-        .options(joinedload(State.cities))
-        .order_by(State.id)
-        .all()
-    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in states:
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
+
         for city in state.cities:
             print("\t{}: {}".format(city.id, city.name))
 
