aa0a1debee5bd2b43ae06f3bb2cfdab58695732f
JephEbuka <jephubakaeze@gmail.com>
2026-08-12 08:23:05 +0100

    Fix catch me request

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the request and follow redirects to catch the server
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+# Send the required request and retrieve the redirected response
+curl -s "$(curl -s -o /dev/null -w '%{redirect_url}' -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me)"
